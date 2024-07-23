bb8d2c523aa087297de23f6e5745977a05a83dd5
qian-yu-db <qian-yu-db>
2024-07-23 06:27:25 +0000

    add data ingestion notebooks

---
+++ Data_Ingestion/00-setup.py
@@ -0,0 +1,142 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC # Create a UC Volume to store synthetic datasets
+
+# COMMAND ----------
+
+# MAGIC %run ./config
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ## Set `reset_all_data` to `True` to clean up existing data
+
+# COMMAND ----------
+
+dbutils.widgets.text("reset_all_data", "false", "Reset Data")
+reset_all_data = dbutils.widgets.get("reset_all_data") == "true"
+
+# COMMAND ----------
+
+import os
+import requests
+
+spark.sql(f'USE CATALOG {catalog};')
+spark.sql(f'USE SCHEMA {db};')
+spark.sql(f'CREATE VOLUME IF NOT EXISTS {volume_name_audio};')
+spark.sql(f'CREATE VOLUME IF NOT EXISTS {volume_name_policies};')
+spark.sql(f'CREATE VOLUME IF NOT EXISTS {volume_name_docs};')
+volume_folder_policy = f"/Volumes/{catalog}/{db}/{volume_name_policies}"
+volume_folder_speech = f"/Volumes/{catalog}/{db}/{volume_name_audio}"
+volume_folder_docs = f"/Volumes/{catalog}/{db}/{volume_name_docs}"
+policy_sub = "Policies"
+audio_sub = "audio_clips"
+transcript_sub = "transcripts_json_data"
+pdf_sub = "pdfs"
+img_sub = "imgs"
+
+# COMMAND ----------
+
+if reset_all_data:
+    try:
+        print("clean volume ...")
+        dbutils.fs.rm(f'{volume_folder_policy}/{policy_sub}', True)
+        dbutils.fs.rm(f'{volume_folder_speech}/{audio_sub}', True)
+        dbutils.fs.rm(f'{volume_folder_speech}/{transcript_sub}', True)
+        dbutils.fs.rm(f'{volume_folder_docs}/{pdf_sub}', True)
+        dbutils.fs.rm(f'{volume_folder_docs}/{img_sub}', True)
+    except Exception as e:
+        print(f'Could not clean folders, they might not exist? {e}')
+
+# COMMAND ----------
+
+import collections
+
+def is_folder_empty(folder):
+  try:
+    return len(dbutils.fs.ls(folder)) == 0
+  except:
+    return True
+  
+
+def get_subdir_name(owner, repo, path):
+    content = requests.get(f'https://api.github.com/repos/{owner}/{repo}/contents/{path}').json()
+    subdirectories = [item for item in content if item['type'] == 'dir']
+    return [subdir['name'] for subdir in subdirectories]
+ 
+
+def download_file_from_git(dest, owner, repo, path):
+    def download_file(url, destination):
+      local_filename = url.split('/')[-1]
+      # NOTE the stream=True parameter below
+      with requests.get(url, stream=True) as r:
+          r.raise_for_status()
+          print('saving '+destination+'/'+local_filename)
+          with open(destination+'/'+local_filename, 'wb') as f:
+              for chunk in r.iter_content(chunk_size=8192): 
+                  f.write(chunk)
+      return local_filename
+
+    if not os.path.exists(dest):
+      os.makedirs(dest)
+    from concurrent.futures import ThreadPoolExecutor
+    files = requests.get(f'https://api.github.com/repos/{owner}/{repo}/contents{path}').json()
+    files = [f['download_url'] for f in files if 'NOTICE' not in f['name']]
+    def download_to_dest(url):
+         download_file(url, dest)
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        collections.deque(executor.map(download_to_dest, files))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ## Download Data from Github to Unity Catalog Volumes
+# MAGIC
+# MAGIC **Note**: To help reduce the notebook running time due to Speech to Text transcription time, we sample 20 conversations only. There are total of 65 conversation available to sample from
+
+# COMMAND ----------
+
+import random
+repo_owner = 'qian-yu-db'
+repo_name = 'Fins-SSA-GenAi-Offerings'
+
+if is_folder_empty(f"{volume_folder_policy}/{policy_sub}") or is_folder_empty(f"{volume_folder_speech}/{audio_sub}"):
+    download_file_from_git(dest=f'{volume_folder_policy}/{policy_sub}', 
+                           owner=repo_owner, 
+                           repo=repo_name,
+                           path="/datasets/insurance_policies")
+
+    download_file_from_git(dest=f'{volume_folder_speech}/{transcript_sub}', 
+                           owner=repo_owner, 
+                           repo=repo_name,
+                           path="/datasets/call_center_audio_transcripts")
+    
+    download_file_from_git(dest=f'{volume_folder_docs}/{pdf_sub}', 
+                           owner=repo_owner, 
+                           repo=repo_name,
+                           path="/datasets/pdfs")
+    
+    download_file_from_git(dest=f'{volume_folder_docs}/{img_sub}', 
+                        owner=repo_owner, 
+                        repo=repo_name,
+                        path="/datasets/imgs")
+    
+    audio_clip_subdirs = get_subdir_name(owner=repo_owner, repo=repo_name, path="datasets/call_center_audio_clips")
+
+    # To help reduce the notebook running time, we sample 20 conversations only
+    audio_clip_subdirs_sample = random.sample(audio_clip_subdirs, k=20)
+    print(f"Download {len(audio_clip_subdirs_sample)} audio conversations")
+    for subdir in audio_clip_subdirs_sample:
+        download_file_from_git(dest=f'{volume_folder_speech}/{audio_sub}/{subdir}', 
+                            owner=repo_owner, 
+                            repo=repo_name, 
+                            path=f"/datasets/call_center_audio_clips/{subdir}")
+else:
+    print("Data already existing. To clean up, run above cell with reset_all_date=True.")
+
+# COMMAND ----------
+
+
